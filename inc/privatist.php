9c2c4353feae2230749c89c84f1e1d0c9963daed
arve0 <arve.seljebu@gmail.com>
2014-08-01 11:01:09 +0200

    privatist: style, fagkoder

--- inc/privatist.php
+++ inc/privatist.php
@@ -15,10 +15,11 @@
         Du slipper obligatorisk oppmøte og trenger kun å gjøre en god eksamen. Du bestemmer omfanget; litt  hjelp eller full oppfølgning hver eneste uke. Er du usikker på hvordan du burde legge opp lesingen, hjelper vi med det også.<br><br>
         Bestiller du en pakke fra oss er vi konkurransedyktige på pris med privatistgymnas. Med privatundervisning vil eleven få mer tid med læreren, som er fordelaktig dersom eleven jobber selvstendig. Hvorfor betale for å sitte i et klasserom, hvis du vet hva du trenger hjelp med? Hos oss betaler du ikke for undervisningen til en hel klasse, men for ditt eget behov.
       </p>
-      <h4>Eksempel på pakketilbud:</h4>
+      <h4>Eksempel på pakketilbud</h4>
       <p class="lead">
         Du begynner å lese 1. september og har eksamen 1. desember. Du får hjelp med pensumet 2 timer hver uke fram til eksamen. Pakkepris: 11700 kr. <a class="goto" href="#kontakt">Kontakt oss</a> for pakketilbud.</a>
-        <br><br>Vi hjelper deg med følgende fag:
+      </p><br>
+      <h4>Privatistfag på videregående vi underviser i</h4><br>
       </p>
       <div class="col-xs-12 col-sm-6">
         <table class="table table-bordered blue">
@@ -32,10 +33,10 @@
             <td>Teoretisk 1T, 2T</td>
           </tr>
           <tr>
-            <td>Samfunnsfag 1S, 2S</td>
+            <td>Samfunnsfag S1, S2</td>
           </tr>
           <tr>
-            <td>Realfag 1R, 2R</td>
+            <td>Realfag R1, R2</td>
           </tr>
           <tr>
             <th>Fysikk 1, 2</th>
