b418e7207592516f66e53234f0f2cf2db0d3ed2c
arve0 <arve.seljebu@gmail.com>
2014-09-16 10:53:38 +0200

    mal for tips

---
+++ htdocs/tips.php
@@ -0,0 +1,7 @@
+<?php
+$tittel = "Leksehjelp matte, fysikk, kjemi, IT";
+include '../inc/head.php';
+include '../inc/tips.php';
+include '../inc/kontakt.php';
+include '../inc/end.php';
+?>
---
+++ inc/tips.php
@@ -0,0 +1,14 @@
+<div id="tips">
+<!-- Artikler -->
+<div class="row artikkel">
+  <div class="col-xs-12">
+    <div class="well clearfix">
+      <h3>Tips</h3>
+      <p class="lead">
+        For å få best utbytte av undervisningen og gode resultater, anbefaler vi dette.
+        <br><br>
+      </p>
+    </div>
+  </div>
+</div>
+</div>
