42e17a4c22f473508ee7cdeeb90299a4f26b152e
arve0 <arve.seljebu@gmail.com>
2014-10-22 21:51:49 +0200

    deploy via ip (vpn)

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git push origin master:laptop-master
-ssh seljebu "cd /opt/privatlaerer && git merge laptop-master"
+ssh 10.10.10.87 "cd /opt/privatlaerer && git merge laptop-master"
 git pull
